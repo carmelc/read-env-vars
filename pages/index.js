0151948349266547316379548d7389b5ebd47dec
carmelc <carmelc@wix.com>
2022-12-05 17:29:44 +0200

    create simple site

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,23 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import { useRouter } from 'next/router'
 
 export default function Home() {
+  const router = useRouter();
+  const queryParams = router.query;
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>format env params</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <Header title="Format env param!" />
+        <textarea style={{width: '100%', height: '100%'}} value={
+          Object.keys(queryParams).map(paramKey => `${paramKey}=${queryParams[paramKey]}`).join('\n')
+        }/>
       </main>
 
       <Footer />
