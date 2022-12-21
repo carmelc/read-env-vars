46616f5231139f1ac12714edd3825096eb8708b2
carmelc <carmelc@wix.com>
2022-12-21 15:42:51 +0200

    fix vercel integration

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
 import FunnelNetlify from './funnel-netlify';
+import FunnelVercel from './funnel-vercel';
 
-export default FunnelNetlify;
+export default process.env.VERCEL ? FunnelVercel : FunnelNetlify;
