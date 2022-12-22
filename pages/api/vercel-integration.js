33135e37d44a730fdc732499f848798dcb441efb
carmelc <carmelc@wix.com>
2022-12-22 10:20:17 +0200

    use custom redirect url env var

--- pages/api/vercel-integration.js
+++ pages/api/vercel-integration.js
@@ -1,27 +1,23 @@
 import qs from 'querystring'
 
 const handler = async function (req, res) {
+    const oauthInput = {
+        client_id: process.env.VERCEL_CLIENT_ID,
+        client_secret: process.env.VERCEL_CLIENT_SECRET,
+        code: req.body.code,
+        redirect_uri: process.env.INTEGRATION_OAUTH_REDIRECT_URL,
+    };
     const result = await fetch('https://api.vercel.com/v2/oauth/access_token', {
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded'
         },
         method: 'POST',
-        body: qs.stringify({
-            client_id: process.env.VERCEL_CLIENT_ID,
-            client_secret: process.env.VERCEL_CLIENT_SECRET,
-            code: req.body.code,
-            redirect_uri: `${process.env.NEXT_PUBLIC_VERCEL_URL}/api/vercel-redirect`
-        })
+        body: qs.stringify(oauthInput)
     });
 
     const body = await result.json();
     if (!body.access_token) {
-        body.debug = {
-            client_id: process.env.VERCEL_CLIENT_ID,
-            client_secret: process.env.VERCEL_CLIENT_SECRET,
-            code: req.body.code,
-            redirect_uri: `${process.env.URL ?? process.env.NEXT_PUBLIC_VERCEL_URL}/api/vercel-redirect`
-        };
+        body.debug = oauthInput;
         res.status(400).json(body);
         return;
     }
